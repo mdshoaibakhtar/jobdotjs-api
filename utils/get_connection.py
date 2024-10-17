6f4a9d4b7b037fc6e688e5cda53f0e9b43d1a5a4
Md Shoaib Akhtar <mdshoaibakhtar966@gmail.com>
2024-10-17 08:15:24 +0530

    comment dotenv

--- utils/get_connection.py
+++ utils/get_connection.py
@@ -2,12 +2,12 @@
 import boto3
 import psycopg2
 from psycopg2.extras import RealDictCursor
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
 
 class GetConnection:
     def get_connection():
-        load_dotenv()
+        # load_dotenv()
         return psycopg2.connect(
             host='ep-divine-block-a4qsr7yt-pooler.us-east-1.aws.neon.tech',
             port='5432',
