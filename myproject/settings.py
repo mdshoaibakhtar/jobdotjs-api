fbdecba426ba087bd17543c9ad5f5e2040876d10
Md Shoaib Akhtar <mdshoaibakhtar966@gmail.com>
2025-04-04 17:57:03 +0530

    Update settings.py file for dotenv

--- myproject/settings.py
+++ myproject/settings.py
@@ -13,11 +13,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
 # from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(dotenv_path=BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
