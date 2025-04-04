8192adea79be241dbf522019d1d97d7f677d326c
Md Shoaib Akhtar <mdshoaibakhtar966@gmail.com>
2025-04-04 22:31:36 +0530

    logger

--- utils/get_connection.py
+++ utils/get_connection.py
@@ -1,26 +1,26 @@
-from pathlib import Path
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
 class GetConnection:
     @staticmethod
     def get_connection():
-        # Explicitly set and log the path to your .env file
-        # env_path = Path(__file__).resolve().parent.parent / ".env"
-        print("Looking for .env at:")
-        load_dotenv(dotenv_path='../.env')
-
+        # Load the environment variables from the .env file
+        print('Load dot env 1')
+        load_dotenv()
+        print('Load dot env 2', os)
+        # Retrieve the environment variables
         db_host = os.getenv("DB_HOST")
         db_port = os.getenv("DB_PORT")
         db_user = os.getenv("DB_USER")
         db_password = os.getenv("DB_PASSWORD")
         db_name = os.getenv("DB_NAME")
+        print('Load dot env 3')
+        print('db_host:', db_host)
+        print('db_password:', db_password)
 
-        print("db_host:", db_host)
-        print("db_password:", db_password)
-
+        # Return the database connection
         return psycopg2.connect(
             host=db_host,
             port=db_port,
