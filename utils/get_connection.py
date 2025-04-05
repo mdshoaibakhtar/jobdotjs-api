1aa1b5a05f71066f78d2a1708eb881f1a10aae31
Md Shoaib Akhtar <mdshoaibakhtar966@gmail.com>
2025-04-05 08:40:10 +0530

    Update env var in vercel

--- utils/get_connection.py
+++ utils/get_connection.py
@@ -1,26 +1,23 @@
 import os
-from dotenv import load_dotenv
 import psycopg2
-from psycopg2.extras import RealDictCursor
+from pathlib import Path
+from dotenv import load_dotenv
 
 class GetConnection:
     @staticmethod
     def get_connection():
-        # Load the environment variables from the .env file
-        print('Load dot env 1')
-        load_dotenv()
-        print('Load dot env 2', os)
-        # Retrieve the environment variables
+        # Only load .env if running locally
+        if os.environ.get("VERCEL_ENV") is None:
+            env_path = Path(__file__).resolve().parent.parent / '.env'
+            load_dotenv(dotenv_path=env_path)
+            print("Loaded .env from local")
+
         db_host = os.getenv("DB_HOST")
         db_port = os.getenv("DB_PORT")
         db_user = os.getenv("DB_USER")
         db_password = os.getenv("DB_PASSWORD")
         db_name = os.getenv("DB_NAME")
-        print('Load dot env 3')
-        print('db_host:', db_host)
-        print('db_password:', db_password)
 
-        # Return the database connection
         return psycopg2.connect(
             host=db_host,
             port=db_port,
